9c47b8621533361b5287a27f292687ef20c2f74a
Johnny Yip <jyippy2012@gmail.com>
2020-09-02 16:10:36 +0100

    release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,10 @@
 class DockingStation
+
+    def release_bike
+    puts "Your bike has been released."
+    end
+
 end 
 
 docking_station = DockingStation.new
+docking_station.release_bike
--- spec/boris_bike_spec.rb
+++ spec/boris_bike_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
 describe DockingStation do
+    it "Testing release_bike" do
+        docking_station = DockingStation.new
+        expect(docking_station.release_bike).to eq "Your bike has been released."
+    end
     
 end
